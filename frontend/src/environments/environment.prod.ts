72eac348b7a7a1a5f0f66fa43783fafeb9b67535
LccSistemas <41525623+LccSistemas@users.noreply.github.com>
2025-12-18 14:46:35 -0300

    Update API URL to Heroku

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://loja-la-luz-api.herokuapp.com/api',
+  apiUrl: 'https://ecommerce-api-calazans-61cda46a9a81.herokuapp.com/api',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'https://ecommerce-api-calazans-61cda46a9a81.herokuapp.com/api',
 };
